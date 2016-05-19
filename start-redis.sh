b14e2d2c916149988339196852a04e1d027c2c94
renealme <renealme@outlook.es>
2016-05-19 15:58:27 -0500

    Create start-redis.sh

---
+++ start-redis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PORT=${PORT}
+REDIS_CONFIGURATION_FILE=/etc/redis.conf
+
+echo "port ${PORT}" >> $REDIS_CONFIGURATION_FILE
+echo "cluster-enabled yes" >> $REDIS_CONFIGURATION_FILE
+echo "cluster-config-file nodes.conf" >> $REDIS_CONFIGURATION_FILE
+echo "cluster-node-timeout 5000" >> $REDIS_CONFIGURATION_FILE
+echo "appendonly yes" >> $REDIS_CONFIGURATION_FILE
+
+/usr/local/bin/redis-server $REDIS_CONFIGURATION_FILE
