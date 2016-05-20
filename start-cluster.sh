871e17268f78aa2b0dc620ac93feffd58798058b
renealme <renealme@outlook.es>
2016-05-20 11:10:06 -0500

    Update start-cluster.sh

--- start-cluster.sh
+++ start-cluster.sh
@@ -7,9 +7,7 @@ M3_PORT=${M3_PORT}
 R1_PORT=${R1_PORT}
 R2_PORT=${R2_PORT}
 R3_PORT=${R3_PORT}
-LOG_FILE=/src/redis-cluster.log
-
-apk add --update supervisor ruby ruby-dev redis && gem install --no-ri --no-rdoc redis
+LOG_FILE=/redis-cluster.log
 
 ruby redis-trib.rb create --replicas 1 ${IP}:${M1_PORT} ${IP}:${M2_PORT} ${IP}:${M3_PORT} ${IP}:${R1_PORT} ${IP}:${R2_PORT} ${IP}:${R3_PORT} >> ${LOG_FILE} 
 tail -f ${LOG_FILE}
