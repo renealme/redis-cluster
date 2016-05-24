db66f83147922ec21a25feb99cec712771a84d30
sapiens83 <sapiens83@gmail.com>
2016-05-24 09:53:22 -0500

    Update start-redis.sh

--- start-redis.sh
+++ start-redis.sh
@@ -3,7 +3,8 @@
 PORT=${PORT}
 REDIS_CONFIGURATION_FILE=/etc/redis.conf
 
-echo "port ${PORT}" > $REDIS_CONFIGURATION_FILE
+echo "bind 0.0.0.0" > $REDIS_CONFIGURATION_FILE
+echo "port ${PORT}" >> $REDIS_CONFIGURATION_FILE
 echo "cluster-enabled yes" >> $REDIS_CONFIGURATION_FILE
 echo "cluster-config-file nodes.conf" >> $REDIS_CONFIGURATION_FILE
 echo "cluster-node-timeout 5000" >> $REDIS_CONFIGURATION_FILE
