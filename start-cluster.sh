4946a20c0f3fac0310a60c37bba41adabe821e35
renealme <renealme@outlook.es>
2016-05-24 10:18:58 -0500

    Update start-cluster.sh

--- start-cluster.sh
+++ start-cluster.sh
@@ -9,5 +9,5 @@ R2_PORT=${R2_PORT}
 R3_PORT=${R3_PORT}
 LOG_FILE=/redis-cluster.log
 
-ruby redis-trib.rb create --replicas 1 ${IP}:${M1_PORT} ${IP}:${M2_PORT} ${IP}:${M3_PORT} ${IP}:${R1_PORT} ${IP}:${R2_PORT} ${IP}:${R3_PORT} >> ${LOG_FILE} 
+echo "yes" | ruby redis-trib.rb create --replicas 1 ${IP}:${M1_PORT} ${IP}:${M2_PORT} ${IP}:${M3_PORT} ${IP}:${R1_PORT} ${IP}:${R2_PORT} ${IP}:${R3_PORT} >> ${LOG_FILE} 
 tail -f ${LOG_FILE}
