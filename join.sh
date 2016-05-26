2829d9d2dcc4a57e4c00ff26f4cbc99dbbb82da4
sapiens83 <sapiens83@gmail.com>
2016-05-26 16:20:08 -0500

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-echo $PORT
 IP="9.7.116.148"
-
 while true
 do
         echo -e "Waiting..."
@@ -11,7 +9,6 @@ do
                 break
         fi
 done
-
 
 for i in 7001 7002 7003 7004 7005 7006
 do
@@ -28,15 +25,15 @@ do
                         res3=`redis-cli -p $i cluster slaves $c | grep -v fail`
                         if [ $res3=="" ]; then
                                 echo -e "c $c"
-                                echo "First Slaveless Master"
                                 portMaster=`redis-cli -p $i cluster nodes | grep master | grep -v fail | grep $c | cut -f2 -d " " | cut -f2 -d :`
+                                echo "-----------------First Slaveless Master $portMaster----------------------"
                                 ruby redis-trib.rb add-node --slave $IP:$PORT $IP:$portMaster
                         fi
                         if [ -n "$portMaster" ];then
                                  break
                         fi
                 done
-                echo -e "$res2"
+               
 
         fi
         echo -e "\n_____________________________________"
