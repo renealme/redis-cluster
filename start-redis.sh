0681e738f60e1dcff9dd21dd062929df285d60aa
sapiens83 <sapiens83@gmail.com>
2016-05-27 12:14:17 -0500

    Update start-redis.sh

--- start-redis.sh
+++ start-redis.sh
@@ -9,6 +9,7 @@ echo "cluster-enabled yes" >> $REDIS_CONFIGURATION_FILE
 echo "cluster-config-file nodes.conf" >> $REDIS_CONFIGURATION_FILE
 echo "cluster-node-timeout 5000" >> $REDIS_CONFIGURATION_FILE
 echo "appendonly yes" >> $REDIS_CONFIGURATION_FILE
+echo "cluster-require-full-coverage no" >> $REDIS_CONFIGURATION_FILE
 LOG_FILE="/redis.log"
 /usr/local/bin/redis-server $REDIS_CONFIGURATION_FILE >> ${LOG_FILE} &
 /bin/join.sh >> ${LOG_FILE} &
