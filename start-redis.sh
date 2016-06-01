621a71d5f979a3f7b5d57b0f45332afd5585e1e0
sapiens83 <sapiens83@gmail.com>
2016-06-01 15:43:14 -0500

    Update start-redis.sh

--- start-redis.sh
+++ start-redis.sh
@@ -12,5 +12,6 @@ echo "appendonly yes" >> $REDIS_CONFIGURATION_FILE
 echo "cluster-require-full-coverage no" >> $REDIS_CONFIGURATION_FILE
 LOG_FILE="/redis.log"
 /usr/local/bin/redis-server $REDIS_CONFIGURATION_FILE >> ${LOG_FILE} &
+/bin/addHostToHaproxy.sh >> ${LOG_FILE} &
 /bin/join.sh >> ${LOG_FILE} &
 tail -n 1000 -f ${LOG_FILE}
