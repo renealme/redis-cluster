704104a61c67b8392086188ceb9f99743c72d99e
sapiens83 <sapiens83@gmail.com>
2016-06-01 15:39:01 -0500

    Create addHostToHaproxy.sh

---
+++ addHostToHaproxy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+Ip=`hostname -I |cut -f1 -d " "`
+echo "IP $Ip"
+HAfile=/etc/haproxy/haproxy.cfg
+SSC=caldelas@caldelas-ThinkCentre-M92p
+tmpFile=/tmp/haproxy.cfg
+scp $SSC:$HAfile $tmpFile
+Ip=`hostname -I | cut -f1 -d " "`
+#PORT=7015
+echo "PORT $PORT"
+##
+exist=`cat ${tmpFile} | grep "server" | grep ":" | grep "$Ip" | grep ":$PORT "| wc -l`
+echo "Exist $exist"
+if [ "$exist" -eq 0 ]; then
+        echo "Does not exist yet on the HAProxy.... Lets add it"
+        echo "server redis_$PORT $Ip:$PORT check inter 1s" >> ${tmpFile}
+        echo "tryng to copy...."
+        scp $tmpFile $SSC:/etc/haproxy
+        tail ${tmpFile}
+        echo "file Copyed..."
+fi
+echo "Done"
