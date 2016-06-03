759f123bd66a50712e1e92fdb8e79399299669ad
sapiens83 <sapiens83@gmail.com>
2016-06-03 16:48:36 -0500

    Update addHostToHaproxy.sh

--- addHostToHaproxy.sh
+++ addHostToHaproxy.sh
@@ -19,20 +19,6 @@ if [ "$exist" -eq 0 ]; then
         echo "tryng to copy...."
         scp $tmpFile $SSC:/etc/haproxy
         echo "server redis_$PORT $Ip:$PORT check inter 1s" |ssh $SSC "cat >> $HAfile"
-        sleep 2
-        ## second try
-        echo "---Second Try---"
-        scp -o StrictHostKeyChecking=no $SSC:$HAfile $tmpFile
-        Ip=`ifconfig | grep "inet addr:9." | cut -f2 -d : | cut -f1 -d " "`
-        exist=`cat ${tmpFile} | grep "server" | grep ":" | grep "$Ip" | grep ":$PORT "| wc -l`
-        echo "Exist $exist"
-        if [ "$exist" -eq 0 ]; then
-                echo "Does not exist yet on the HAProxy.... Lets add it"
-                echo "server redis_$PORT $Ip:$PORT check inter 1s" >> ${tmpFile}
-                echo "tryng to copy...."
-                scp $tmpFile $SSC:/etc/haproxy
-        fi
-        ##
         tail ${tmpFile}
         echo "file Copyed..."
 fi
