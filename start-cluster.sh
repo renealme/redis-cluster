e7b5982b68ea708c809db8b5875b95d852736264
sapiens83 <sapiens83@gmail.com>
2016-06-23 11:36:33 -0500

    Update start-cluster.sh

--- start-cluster.sh
+++ start-cluster.sh
@@ -12,7 +12,7 @@ LOG_FILE=/redis-cluster.log
 tmpFile=/tmp/haproxy.cfg
 HAfile=/etc/haproxy/haproxy.cfg.tmp
 SSH="caldelas@caldelas-thinkcentre-m92p.gdl.mex.ibm.com"
-scp -o StrictHostKeyChecking=no $SSC:$HAfile $tmpFile
+scp -o StrictHostKeyChecking=no $SSH:$HAfile $tmpFile
 result=`cat $tmpFile | grep "^server" | cut -f3 -d " "`
 toCluster=""
 for i in $result; do
