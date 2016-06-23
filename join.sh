5f1e4d70c43ee0d2e030be6ffdbed897b4d3be7a
sapiens83 <sapiens83@gmail.com>
2016-06-23 16:24:56 -0500

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,39 +1,68 @@
 #!/bin/sh
 
-IP="9.7.116.148"
-while true
-do
-        echo -e "Waiting..."
-        isUp=`redis-cli -p $PORT info`
-        if [ "$isUp" != "" ]; then
-                break
-        fi
-done
+#IP="9.7.116.148"
+#Declaring Variables
+tmpFile=/tmp/haproxy.cfg
+HAfile=/etc/haproxy/haproxy.cfg.tmp
+SSH="caldelas@caldelas-thinkcentre-m92p.gdl.mex.ibm.com"
 
-for i in 7001 7002 7003 7004 7005 7006
-do
-        echo $i
-        res=`redis-cli -p $i cluster slots`
-        #echo $res
-        if [ $res=="" ]; then
-                echo "Empty Cluster"
+#get the Docker host IP
+IP=`ifconfig |  grep -A 1 em1 | grep "addr:" | cut -f2 -d : | cut -f1 -d " "`
+if [ $IP=="" ]; then
+        IP=`ifconfig |  grep -A 1 eth0 | grep "addr:" | cut -f2 -d : | cut -f1 -d " "`
+fi
+##
+
+#whait until the redis for this container get ready
+#while true
+#do
+ #       echo -e "Waiting..."
+  #      isUp=`redis-cli -h $Ip -p $PORT info`
+   #     if [ "$isUp" != "" ]; then
+    #            break
+     #   fi
+#done
+##
+echo Ip $IP
+
+#copy remote haproxy file to a local dir
+scp -o StrictHostKeyChecking=no $SSH:$HAfile $tmpFile
+result=`cat $tmpFile | grep "^server" | cut -f3 -d " "`
+#cat $tmpFile
+toCluster=""
+
+
+for i in $result; do
+        Ip=`echo $i | cut -f1 -d:`
+        Port=`echo $i | cut -f2 -d:`
+        echo  -e " i = $i "
+        echo IP $Ip
+        echo Port $Port
+        toCluster="$toCluster $Ip:$Port"
+
+	echo toCluster $toCluster
+
+        #echo $i
+        res=`redis-cli -h $Ip -p $Port cluster slots`
+        echo  res $res
+        if [ $res=="" ] ; then
+                echo "Empty Cluster " $res  
         else
                 echo "Starting......"
-                res2=`redis-cli -p $i cluster nodes | grep master | grep -v fail | cut -f1 -d " "`
+                res2=`redis-cli -h $Ip -p $Port cluster nodes | grep master | grep -v fail | cut -f1 -d " "`
                 for c in $res2
                 do
-                        res3=`redis-cli -p $i cluster slaves $c | grep -v fail`
-                        if [ $res3=="" ]; then
-                                portMaster=`redis-cli -p $i cluster nodes | grep master | grep -v fail | grep $c | cut -f2 -d " " | cut -f2 -d :`
+                        res3=`redis-cli -h $Ip -p $Port cluster slaves $c | grep -v fail`
+                        if [ ${res3}=="" ]; then
+                                portMaster=`redis-cli -h $Ip -p $Port cluster nodes | grep master | grep -v fail | grep $c | cut -f2 -d " " | cut -f2 -d :`
                                 echo "-----------------First Slaveless Master $portMaster----------------------"
-                                ruby redis-trib.rb add-node --slave $IP:$PORT $IP:$portMaster
+                                # add anode as slave
+				ruby redis-trib.rb add-node --slave $IP:$PORT $IP:$portMaster
                         fi
                         if [ -n "$portMaster" ];then
                                  break
                         fi
                 done
-               
-
         fi
         echo -e "\n_____________________________________"
         if [ -n "$portMaster" ]; then
